b9b040cce2b88f49a08ccd16c0983738e5deffba
Admin <vinhblue97@gmail.com>
2022-06-11 14:46:19 +0700

    init project

---
+++ src/index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const express = require('express');
+const handlebars = require('express-handlebars');
+const path = require('path');
+const app = express();
+const morgan = require('morgan')
+const port = 3000;
+
+/**
+ * use morgan in project ~ HTTP Logger
+ */
+app.use(morgan('combined'))
+
+/**
+ * Rendering template engine setup
+ */
+app.engine('.hbs', handlebars.engine({extname: '.hbs', defaultLayout: "main"}));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'resource/views'));
+
+
+app.get('/', function(req, res){
+    res.render('home');
+})
+
+app.listen(port, function(error){
+    if (error) {
+        console.log("Something went wrong");
+    }
+    console.log("server is running port:  " + port);
+})
